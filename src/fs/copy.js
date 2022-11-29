7261f3bb98e43b4e8d802a7b768efe7a1fb19853
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-11-29 20:13:24 +0300

    feat: rename file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,17 +1,19 @@
-import { opendir, copyFile, mkdir  } from 'node:fs/promises';
-import { isPathExist } from './utils/isPathExist.js';
-import { errorMessage, sourceDirectory } from './utils/constants.js';
+import { opendir, copyFile, mkdir } from "node:fs/promises";
+import { isPathExist } from "./utils/isPathExist.js";
+import { errorMessage, sourceDirectory } from "./utils/constants.js";
 
-const destinationDirectory = 'files_copy';
+const destinationDirectory = "files_copy";
 const path = `src/fs/`;
 
 const copy = async () => {
   try {
-    const isDestinationDirExist = await isPathExist(`${path}/${destinationDirectory}`);
+    const isDestinationDirExist = await isPathExist(
+      `${path}/${destinationDirectory}`
+    );
     const isSourceDirExist = await isPathExist(`${path}/${sourceDirectory}`);
 
     if (!isSourceDirExist || isDestinationDirExist) {
-        throw new Error(errorMessage)
+      throw new Error(errorMessage);
     }
 
     await mkdir(`${path}/${destinationDirectory}`);
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import { rename as renameFile } from 'node:fs/promises';
+import { isPathExist } from './utils/isPathExist.js';
+import { errorMessage } from './utils/constants.js';
+
+const sourceFileName = 'wrongFilename.txt';
+const destinationFileName = 'properFilename.md';
+const path = `src/fs/files`;
+
 const rename = async () => {
-    // Write your code here 
+  const isDestinationFileExist = await isPathExist(
+    `${path}/${destinationFileName}`
+  );
+  const isSourceFileExist = await isPathExist(
+    `${path}/${sourceFileName}`
+  );
+
+  if (!isSourceFileExist || isDestinationFileExist) {
+    throw new Error(errorMessage);
+  }
+
+  try {
+    await renameFile(
+      `${path}/${sourceFileName}`,
+      `${path}/${destinationFileName}`
+    );
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await rename();
