1cb01062a2bcbd3a22b4f07a80aa87fa78a4584d
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-11-29 21:12:14 +0300

    feat: parse env file

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssKeys = Object.keys(process.env)
+    .filter((key) => !key.search(/^(RSS_)/))
+    .map((key) => `${key}=${process.env[key]}`)
+    .join("; ");
+
+  console.log(rssKeys);
 };
 
 parseEnv();
