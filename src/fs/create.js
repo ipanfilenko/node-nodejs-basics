6920a6b11df3189d13671d441784afa4b9e93137
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-11-29 18:03:19 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import { access, appendFile } from 'node:fs/promises';
+
+const directory = 'files';
+const fileName = 'fresh.txt';
+const textInFile = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+const successMessage = 'File created';
+const path = `src/${directory}/${fileName}`;
+
 const create = async () => {
-    // Write your code here 
+  let isFileExist = false;
+
+  try {
+    await access(path);
+    isFileExist = true;
+  } catch {}
+
+
+  if (isFileExist) {
+    throw new Error(errorMessage);
+  }
+
+  try {
+    await appendFile(path, textInFile);
+    console.log(successMessage);
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await create();
