0a6bc440f156d02c871911d2ebd79fed2b911ace
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-11-29 20:45:08 +0300

    feat: read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { readFile } from 'node:fs/promises';
+import { isPathExist } from "./utils/isPathExist.js";
+import { errorMessage } from "./utils/constants.js";
+
+const path = `src/fs/files/fileToRead.txt`;
+
 const read = async () => {
-    // Write your code here 
+  const isSourceFileExist = await isPathExist(path);
+
+  if (!isSourceFileExist) {
+    throw new Error(errorMessage);
+  }
+
+  try {
+    const contents = await readFile(path, { encoding: "utf8" });
+    console.log(contents);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,18 +1,16 @@
-import { rename as renameFile } from 'node:fs/promises';
-import { isPathExist } from './utils/isPathExist.js';
-import { errorMessage } from './utils/constants.js';
+import { rename as renameFile } from "node:fs/promises";
+import { isPathExist } from "./utils/isPathExist.js";
+import { errorMessage } from "./utils/constants.js";
 
-const sourceFileName = 'wrongFilename.txt';
-const destinationFileName = 'properFilename.md';
+const sourceFileName = "wrongFilename.txt";
+const destinationFileName = "properFilename.md";
 const path = `src/fs/files`;
 
 const rename = async () => {
   const isDestinationFileExist = await isPathExist(
     `${path}/${destinationFileName}`
   );
-  const isSourceFileExist = await isPathExist(
-    `${path}/${sourceFileName}`
-  );
+  const isSourceFileExist = await isPathExist(`${path}/${sourceFileName}`);
 
   if (!isSourceFileExist || isDestinationFileExist) {
     throw new Error(errorMessage);
