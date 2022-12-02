dca2798596ac7a03f8fa453f76a2b7a0e546cf73
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-12-02 15:04:44 +0300

    feat: calculate hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createReadStream } from 'node:fs';
+const { createHash } = await import('node:crypto');
+
+const path = 'src/hash/files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const inputFile = createReadStream(path);
+
+  inputFile.on('readable', () => {
+    const data = inputFile.read();
+
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(`${hash.digest('hex')}`);
+    }
+  });
 };
 
 await calculateHash();
