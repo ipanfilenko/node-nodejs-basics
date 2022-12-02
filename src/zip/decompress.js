84e8d919d9dcf68682266d7f131acaf579b22f85
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-12-02 17:05:24 +0300

    feat: compress and decompress file

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+
+const gunzip = createGunzip();
+const source = createReadStream('src/zip/files/archive.gz');
+const destination = createWriteStream('src/zip/files/fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await decompress();
