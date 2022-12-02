6f5af017ebef157b4aff8d600dde272a5bee6fcc
ipanfilenko <Igor.Panfilenko@wolterskluwer.com>
2022-12-02 16:46:17 +0300

    feat: work with streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { open } from 'node:fs/promises';
+import { stdout } from 'node:process';
+
+const path = 'src/streams/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fd = await open(path);
+        fd.createReadStream().pipe(stdout);
+    } catch {
+        console.error('Can not read source file');
+    } 
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+
+const reverseString = new Transform({
+  transform(chunk, _encoding, callback) {
+    callback(null, String(chunk).split('').reverse().join('') + '\n');
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  stdin.pipe(reverseString).pipe(stdout);
 };
 
 await transform();
